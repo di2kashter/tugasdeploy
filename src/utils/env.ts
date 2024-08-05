7324427240449097333c670b61ab4e0659ecb9a6
Didik Setiawan <imdidiksetiawan@gmail.com>
2024-08-05 15:08:52 +0700

    update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "472741184432438";
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY  || "";
 export const CLOUDINARY_API_SECRET: string =
-  process.env.CLOUDINARY_API_SECRET || "QJ4Hm2lQxqEwTrStJYdd0xth8Yc";
+  process.env.CLOUDINARY_API_SECRET  || "";
 export const CLOUDINARY_CLOUD_NAME: string =
-  process.env.CLOUDINARY_CLOUD_NAME || "dvbwty9ly";
-export const DATABASE_URL: string = "mongodb+srv://di2kashter:V75syoynSG1XfgdH@belajar-be-nodejs.yuu3smx.mongodb.net/?retryWrites=true&w=majority&appName=belajar-be-nodejs";
+  process.env.CLOUDINARY_CLOUD_NAME || "";
+export const DATABASE_URL: string =process.env.DB || "";
 
 
 
-export const SECRET: string = process.env.SECRET || "secret";
+export const SECRET: string = process.env.SECRET || "";
